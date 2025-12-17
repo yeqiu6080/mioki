a078365d7427fd65a73547363bc6a3e5c85711c3
Viki <hi@viki.moe>
2025-12-17 12:39:00 +0800

    chore: 完善 segment 和 message 消息片段类型

--- packages/napcat-sdk/src/onebot.ts
+++ packages/napcat-sdk/src/onebot.ts
@@ -7,7 +7,7 @@ export interface MediaProps {
   file_unique: string
 }
 
-export type MessageElement =
+export type RecMessageElement =
   | { type: 'text'; text: string }
   | { type: 'at'; qq: 'all' | (string & {}) }
   | { type: 'reply'; id: string }
@@ -15,21 +15,60 @@ export type MessageElement =
   | ({ type: 'image'; summary?: string; sub_type?: string } & MediaProps)
   | ({ type: 'record' } & MediaProps)
   | ({ type: 'video' } & MediaProps)
+  | { type: 'rps'; result: string }
+  | { type: 'dice'; result: string }
+  | { type: 'shake' }
+  | { type: 'poke' }
+  | { type: 'share' }
+  | { type: 'location' }
+  | { type: 'forward'; id: string; content: [] }
+  | { type: 'json'; data: string }
+  | ({ type: 'file' } & MediaProps)
+  | { type: 'markdown' }
+  | { type: 'lightapp' }
 
-export type NormalizedElementToSend =
-  | { type: 'text'; data: { text: string } }
-  | { type: 'at'; data: { qq: 'all' | (string & {}) | number } }
-  | { type: 'reply'; data: { id: string } }
-  | { type: 'face'; data: { id: number } }
-  | { type: 'bface'; data: { id: number } }
-  | { type: 'image'; data: { file: string; name?: string; summary?: string; sub_type?: string } }
-  | { type: 'video'; data: { file: string; name?: string; thumb?: string } }
-  | { type: 'record'; data: { file: string; name?: string } }
+export type NormalizedElement =
+  | { type: 'text'; text: string }
+  | { type: 'at'; qq: 'all' | (string & {}) | number }
+  | { type: 'reply'; id: string }
+  | {
+      type: 'mface'
+      id: number
+      key: string
+      emoji_id: string
+      emoji_package_id: string
+      summary?: string
+    }
+  | { type: 'face'; id: number }
+  | { type: 'bface'; id: number }
+  | { type: 'image'; file: string; name?: string; summary?: string; sub_type?: string }
+  | { type: 'video'; file: string; name?: string; thumb?: string }
+  | { type: 'record'; file: string; name?: string }
+  | { type: 'contact'; sub_type: 'qq' | 'group'; id: string }
+  | { type: 'poke' }
+  | ({ type: 'music' } & (
+      | { platform: 'qq' | '163' | 'kugou' | 'migu' | 'kuwo'; id: string }
+      | { platform: 'custom'; url: string; audio: string; title: string; image?: string; singer?: string }
+    ))
+  | { type: 'forward'; id: string }
+  | ({
+      type: 'node'
+      user_id?: string
+      nickname?: string
+    } & ({ id: string } | { content: Exclude<NormalizedElement, { type: 'node' }>[] }))
+  | { type: 'json'; data: string }
+  | {
+      type: 'file'
+      name?: string
+      file: string
+    }
+  | { type: 'markdown' }
+  | { type: 'lightapp' }
 
-type FlattenData<T extends { type: string }> = T extends { data: infer U } ? U & { type: T['type'] } : never
+export type WrapData<T extends { type: string }> = { type: T['type']; data: Omit<T, 'type'> }
+export type FlattenData<T extends { type: string }> = T extends { data: infer U } ? U & { type: T['type'] } : never
 
-export type NormalizedElement = FlattenData<NormalizedElementToSend>
-
+export type NormalizedElementToSend = WrapData<NormalizedElement>
 export type Sendable = string | NormalizedElement
 
 export type PostType = 'meta_event' | 'message'
@@ -56,7 +95,7 @@ export type MessageEventBase<T extends MessageType, U extends object> = U &
       real_id: number
       real_seq: number
       raw_message: string
-      message: MessageElement[]
+      message: RecMessageElement[]
       reply: Reply
     }
   >
--- packages/napcat-sdk/src/segment.ts
+++ packages/napcat-sdk/src/segment.ts
@@ -26,4 +26,33 @@ export const segment = {
   /** 创建一个视频消息片段 */
   video: (file: string, options?: Omit<ExtractByType<Element, 'video'>, 'type' | 'file'>): Element =>
     createSegment('video', { file, ...options }),
+  /** 创建一个动态表情消息片段 */
+  mface: (options: Omit<ExtractByType<Element, 'mface'>, 'type'>): Element => createSegment('mface', { ...options }),
+  /** 创建一个大表情消息片段 */
+  bface: (id: number): Element => createSegment('bface', { id }),
+  /** 创建一个 联系人/群 分享消息片段 */
+  contact: (type: 'qq' | 'group', id: string): Element => createSegment('contact', { id, sub_type: type }),
+  /** 创建一个戳一戳消息片段 */
+  poke: (): Element => createSegment('poke', {}),
+  /** 创建一个音乐消息片段 */
+  music: (platform: 'qq' | '163' | 'kugou' | 'migu' | 'kuwo', id: string): Element =>
+    createSegment('music', { platform, id }),
+  /** 创建一个自定义音乐消息片段 */
+  musicCustom: (
+    title: string,
+    audio: string,
+    url: string,
+    options?: Omit<ExtractByType<Element, 'music'>, 'type' | 'platform' | 'url' | 'audio' | 'title'>,
+  ): Element => createSegment('music', { platform: 'custom', url, audio, title, ...options }),
+  /** 创建一个合并转发消息片段 */
+  forward: (id: string): Element => createSegment('forward', { id }),
+  /** 创建一个 JSON 消息片段 */
+  json: (data: string): Element => createSegment('json', { data }),
+  /** 创建一个文件消息片段 */
+  file: (file: string, options?: Omit<ExtractByType<Element, 'file'>, 'type' | 'file'>): Element =>
+    createSegment('file', { file, ...options }),
+  /** 创建一个 Markdown 消息片段 */
+  markdown: (): Element => createSegment('markdown', {}),
+  /** 创建一个轻应用消息片段 */
+  lightapp: (): Element => createSegment('lightapp', {}),
 }
