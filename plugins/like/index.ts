1beff0c16bbb3b0541b1d43c3dce8b907461723a
Viki <hi@viki.moe>
2025-12-20 22:55:33 +0800

    chore: 优化插件

--- plugins/like/index.ts
+++ plugins/like/index.ts
@@ -3,12 +3,17 @@ import { definePlugin } from 'mioki'
 export default definePlugin({
   name: 'like',
   version: '1.0.0',
-  async setup(ctx) {
-    ctx.handle('message.group', async (event) => {
-      if (event.raw_message === '赞我') {
-        const isOK = await ctx.bot.sendLike(event.sender.user_id, 10)
-        await event.addReaction(isOK ? '66' : '67')
-      }
+  description: '名片赞插件',
+  setup: (ctx) => {
+    ctx.handle('message.group', async (e) => {
+      ctx.match(e, {
+        赞我: async () => {
+          let count = 0
+          while (await ctx.bot.sendLike(e.sender.user_id, 10)) count += 10
+          await e.addReaction(count > 0 ? '66' : '67')
+          // return count > 0 ? `赞了你 ${count} 下` : '今天点过了，明天再来'
+        },
+      })
     })
   },
 })
